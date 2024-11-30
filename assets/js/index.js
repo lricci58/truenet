9562b5ecccc35aabd139305b5e53d776dee9f1f4
Lucas Ricci <lucric2001@gmail.com>
2024-11-29 21:45:07 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,39 +2,39 @@ import * as async from './modules/async.js';
 import documentChecker from './modules/document-checker.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-    // const topNavbar = async.get({
-    //     url: '/includes/topnav.html',
-    //     method: 'POST'
-    // });
+    const topNavbar = async.get({
+        url: '/includes/topnav.html',
+        method: 'POST'
+    });
 
-    // const sideNav = async.get({
-    //     url: '/includes/sidenav.html',
-    //     method: 'POST'
-    // });
+    const sideNav = async.get({
+        url: '/includes/sidenav.html',
+        method: 'POST'
+    });
 
-    // const footer = async.get({
-    //     url: '/includes/footer.html',
-    //     method: 'POST'
-    // });
+    const footer = async.get({
+        url: '/includes/footer.html',
+        method: 'POST'
+    });
 
     const basePromiseList = {
-        // 'top_navbar': topNavbar,
-        // 'side_nav': sideNav,
-        // 'footer': footer
+        'top_navbar': topNavbar,
+        'side_nav': sideNav,
+        'footer': footer
     }
 
     async.waitForAll(basePromiseList).then((result) => {
-        // document.getElementById('topnav').innerHTML = result['top_navbar'];
-        // document.getElementById('sidenav').innerHTML = result['side_nav'];
-        // document.getElementById('footer').innerHTML = result['footer'];
+        document.getElementById('topnav').innerHTML = result['top_navbar'];
+        document.getElementById('sidenav').innerHTML = result['side_nav'];
+        document.getElementById('footer').innerHTML = result['footer'];
 
-        // checkActiveTab();
+        checkActiveTab();
 
-        // const burgerIcon = document.getElementById('topnav-burger');
-        // burgerIcon.addEventListener('mousedown', function () { sidenavOpen(); });
+        const burgerIcon = document.getElementById('topnav-burger');
+        burgerIcon.addEventListener('mousedown', function () { sidenavOpen(); });
 
-        // const notificationIcon = document.getElementById('topnav-notification');
-        // notificationIcon.addEventListener('mousedown', function () { notificationPopupOpen(); });
+        const notificationIcon = document.getElementById('topnav-notification');
+        notificationIcon.addEventListener('mousedown', function () { notificationPopupOpen(); });
 
         documentChecker.setReady();
     });
